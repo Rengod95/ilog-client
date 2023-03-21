c28379f3b1e81baac660f0eae2751336b8a1cb0b
Rengod95 <aorwn970@gmail.com>
2023-03-21 14:03:21 +0900

    feat: implement Flex.tsx
    반복적인 flex css style을 줄이기 위해 polymorphic한 Flex컴포넌트를 제작합니다.
    
    styled() 의 인자로 전달하는 등 대부분의 컴포넌트 타입과 호환되며 단독 컴포넌트로서도 사용 가능합니다.
    
    preset을 통해 지정된 flex style을 사용할 수 있으며 custom style 또한 적용 가능합니다.
    
    as 를 통해 원하는 element로 전환할 수 있습니다.

--- src/util/File.ts
+++ src/util/File.ts
@@ -4,22 +4,27 @@ import { remark } from 'remark';
 import html from 'remark-html';
 import fs from 'fs';
 
+//디렉토리 내의 모든 파일 이름 반환
 export const getFileNames = (directory: string): string[] => {
   return fs.readdirSync(directory);
 };
 
+// md 파일 이름 분리
 export const convertFileNameToSlug = (fileName: string): string => {
   return fileName.replace(/\.md$/, '');
 };
 
+// 디렉토리 주소와 마크다운 파일 이름을 통해 최종 md파일의 주소 가져오기
 export const getPostPathBySlug = (directory: string, slug: string) => {
   return path.join(directory, `${slug}.md`);
 };
 
+//주소를 통해 파일 불러오기
 export const getPostFileByPath = (path: string) => {
   return fs.readFileSync(path, 'utf-8');
 };
 
+//frontMatter 추출하기
 export const getMatterResultWithFile = (
   file:
     | string
@@ -30,6 +35,7 @@ export const getMatterResultWithFile = (
   return matter(file);
 };
 
+//마크다운 데이터를 HTML로 파싱
 export const convertMarkdownToHTML = async (result: any) => {
   const processedContent = await remark().use(html).process(result);
   return processedContent.toString();
