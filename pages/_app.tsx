110002fa18c2f9209bcfc21979ba71d321f198ca
Rengod95 <aorwn970@gmail.com>
2023-03-22 02:44:49 +0900

    feat: modify application layout
    어플리케이션의 기본 레이아웃 구조를 수정합니다.
    
    * Config.css를 통해 css initialize
    * Wrapper 역할의 Layout 컴포넌트를 추가
    * AppHeader, AppContent로 기본 섹션 분리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
-import { Layout } from '@/components/layout';
-import styled from '@emotion/styled';
+import { HeaderContent } from '@/components/commons/HeaderContent';
+import { Layout } from '@/components/App';
 import { AppProps } from 'next/app';
+import { ThemeProvider } from '@emotion/react';
 import React from 'react';
+import { DEFAULT_THEME } from '@/styles/Theme';
+import '@/styles/Config.css';
+
+import { AppHeader } from '@/components/App/AppHeader';
+import { AppContent } from '@/components/App/AppContent';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={DEFAULT_THEME}>
+      <Layout>
+        <AppHeader>
+          <HeaderContent />
+        </AppHeader>
+        <AppContent>
+          <Component {...pageProps} />
+        </AppContent>
+      </Layout>
+    </ThemeProvider>
   );
 }
