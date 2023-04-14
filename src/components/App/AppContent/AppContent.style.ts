cd1b16158123565f2e9a0c7e748381c72726bfef
Rengod95 <aorwn970@gmail.com>
2023-04-15 02:51:43 +0900

    fix : a tag no react fix with router

--- src/components/App/AppContent/AppContent.style.ts
+++ src/components/App/AppContent/AppContent.style.ts
@@ -3,12 +3,13 @@ import { Flex } from '@/components/UI/FlexBox';
 
 export const Root = styled.main`
   width: 100%;
-  height: auto;
+
   margin-top: 64px;
 `;
 
 export const Wrapper = styled(Flex)`
   margin: 0 auto;
+  height: 100%;
   width: 100%;
   max-width: 55rem;
 `;
--- src/components/App/AppFooter/FooterContent/FooterContent.tsx
+++ src/components/App/AppFooter/FooterContent/FooterContent.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import * as S from './FooterContent.style';
 import { Chip } from '@/components/UI/Chip';
+import Link from 'next/link';
+import { useNavigation } from '@/util/hooks/UseNavigation.hook';
 
 export const DISCORD_LINK = 'https://discord.gg/abrhZFVcrp';
 export const KAKAOTALK_LINK = 'https://open.kakao.com/o/sgaSi4ef';
 export const FooterContent = () => {
+  const { navigateTo } = useNavigation();
   return (
     <S.GapWrapper flex='columnCenter'>
       <h2>Contact me</h2>
       <S.GapWrapper flex='rowCenter'>
-        <Chip variant='outlined'>
-          <a href={KAKAOTALK_LINK} target={'_blank'} rel='noopener noreferrer'>
-            KakaoTalk
-          </a>
+        <Chip variant='outlined' onClick={() => navigateTo(KAKAOTALK_LINK)}>
+          Kakao Talk
         </Chip>
-        <Chip variant='outlined'>
-          <a href={DISCORD_LINK} target={'_blank'} rel='noopener noreferrer'>
-            Discord
-          </a>
+        <Chip variant='outlined' onClick={() => navigateTo(DISCORD_LINK)}>
+          Discord
         </Chip>
       </S.GapWrapper>
       <h3>Email : aorwn970@gmail.com</h3>
